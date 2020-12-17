5064f3a08cfc29962cb194c3983bb3d3353fac99
elliott lockwood <lockwood721@gmail.com>
2020-12-17 16:58:17 -0600

    store cs update

--- PizzaWorld.Domain/Models/Store.cs
+++ PizzaWorld.Domain/Models/Store.cs
@@ -1,7 +1,33 @@
+using System.Collections.Generic;
+
 namespace PizzaWorld.Domian.Models 
 {
     public class Store
     {
         
+        public List<Order> Orders { get; set; }
+        
+        
+        // CRUD
+        void CreateOrder()
+        {
+            var O = new Order();
+            Orders.Add(new Order());
+        }
+
+        bool DeleteOrder(Order order)
+        {
+            try
+            {
+                Orders.Remove(order);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
+
